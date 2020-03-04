6900b0e50b44cca75349e1116679ebb77fcb15fa
ashton-martin <ashton.martin191@gmail.com>
2020-03-04 05:11:48 +0000

    fixed portfolio page

--- projects/runtime/js/init.js
+++ projects/runtime/js/init.js
@@ -32,11 +32,12 @@ var init = function (window) {
     ground = opspark.makeGround(app);
 
     // TODO 2 : add background
-
+    var background = opspark.makeBackground(app,ground);
+    view.addChild(background);
     
-    var help = draw.textfield('MOVES || up: jump | right: flying jump | down: duck | space: fire | q self destruct!', 
+    var help = draw.textfield('MOVES || up: jump | right: flying jump | down: duck | space: fire | q your mom!', 
         '20px Arial',
-        '#ccc', 'left');
+        'grey', 'left');
     help.x = 10;
     help.y = ground.y + ground.getBounds().height + 10;
     view.addChild(help);
@@ -71,10 +72,12 @@ var init = function (window) {
     }
     
     // TODO 1 : add a heads-up display to game
-
-
+    var hud = opspark.makeHud();
+    view.addChild(hud);
+    window.hud = hud;
     // TODO 6 : create game manager
-
+    var game = opspark.createGameManager(app,hud);
+    opspark.runLevelInGame(game);
 
 };
 
